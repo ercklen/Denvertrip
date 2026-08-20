d22d50e4609f3401d3abc46be19c9d0c011da55e
er0ckl3en <mohamedibourk89@gmail.com>
2026-08-20 06:20:17 +0100

    Fix: configure high-resolution Carto Dark Matter raster tiles for interactive map

--- src/components/ui/mapcn-marker-tooltip.tsx
+++ src/components/ui/mapcn-marker-tooltip.tsx
@@ -22,8 +22,56 @@ function cn(...inputs: Array<string | false | null | undefined>) {
 }
 
 const defaultStyles = {
-  dark: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
-  light: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
+  dark: {
+    version: 8,
+    sources: {
+      "carto-dark": {
+        type: "raster",
+        tiles: [
+          "https://a.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}@2x.png",
+          "https://b.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}@2x.png",
+          "https://c.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}@2x.png",
+          "https://d.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}@2x.png",
+        ],
+        tileSize: 256,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      },
+    },
+    layers: [
+      {
+        id: "carto-dark-layer",
+        type: "raster",
+        source: "carto-dark",
+        minzoom: 0,
+        maxzoom: 20,
+      },
+    ],
+  } as MapLibreGL.StyleSpecification,
+  light: {
+    version: 8,
+    sources: {
+      "carto-light": {
+        type: "raster",
+        tiles: [
+          "https://a.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}@2x.png",
+          "https://b.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}@2x.png",
+          "https://c.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}@2x.png",
+          "https://d.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}@2x.png",
+        ],
+        tileSize: 256,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      },
+    },
+    layers: [
+      {
+        id: "carto-light-layer",
+        type: "raster",
+        source: "carto-light",
+        minzoom: 0,
+        maxzoom: 20,
+      },
+    ],
+  } as MapLibreGL.StyleSpecification,
 };
 
 type Theme = "light" | "dark";
